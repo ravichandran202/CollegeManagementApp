03b0252bf52f4c252832bba2b7d3089236bd4ba1
Ravichandran T S <ravichandrants202@gmail.com>
2023-11-01 20:56:41 +0530

    Added signin and signup page

--- collegeproject/collegeapp/urls.py
+++ collegeproject/collegeapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.home, name="home")
+    path("",views.home_page, name="home"),
+    path("signin",views.signin_page, name="signin"),
+    path("signup",views.signup_page, name="signup")
 ]
--- collegeproject/collegeapp/views.py
+++ collegeproject/collegeapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def home(request):
+def home_page(request):
     return render(request,"index.html")
+
+def signin_page(request):
+    return render(request, "signin.html")
+
+def signup_page(request):
+    return render(request, "signup.html")
