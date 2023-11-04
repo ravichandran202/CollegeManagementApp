dc610cd793345681d1275dd52c3a7b57f00b8b56
Ravichandran T S <ravichandrants202@gmail.com>
2023-11-04 19:47:11 +0530

    Added password patters verification in - signup page

--- collegeproject/collegeapp/views.py
+++ collegeproject/collegeapp/views.py
@@ -6,6 +6,49 @@ from django.contrib import messages
 
 from django.http import HttpResponse
 # Create your views here.
+
+
+import re
+
+def is_valid_password(request,password):
+    # Check if the password is at least 8 characters long
+    is_valid = True
+    if len(password) < 6:
+        messages.error(request, 'Password should contains minimum 8 characters')
+        is_valid = False
+
+    # Check if the password contains at least one uppercase letter (A-Z)
+    if not re.search(r'[A-Z]', password):
+        messages.error(request, 'Password should Include at least one uppercase letter (A-Z)')
+        is_valid = False
+
+    # Check if the password contains at least one lowercase letter (a-z)
+    if not re.search(r'[a-z]', password):
+        messages.error(request, 'Password should Include at least one lowercase letter (A-Z)')
+        is_valid = False
+
+    # Check if the password contains at least one number (0-9)
+    if not re.search(r'[0-9]', password):
+        messages.error(request, 'Password should Include at least one number (0-9)')
+        is_valid = False
+
+    # Check if the password contains at least one special character
+    special_characters = r'[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]'
+    if not re.search(special_characters, password):
+        messages.error(request, 'Include at least one special character')
+        is_valid = False
+
+    # Check if the password contains common words or personal information
+    common_words = ["password", "123456", "admin"]  # Add more common words here
+    for common_word in common_words:
+        if common_word in password.lower():
+            messages.error(request, 'Avoid common words and personal information')
+            is_valid = False
+
+    # All criteria are met, the password is valid
+    return is_valid
+
+
 def home_page(request):
     return render(request,"index.html")
 
@@ -36,6 +79,8 @@ def signup_page(request):
                 return redirect('signup')
             elif User.objects.filter(username=username).exists():
                 messages.error(request, "username Already Exists")
+                return redirect('signup')
+            if not is_valid_password(request,password1):
                 return redirect('signup')
             else:
                 user = User.objects.create_user(username, email, password1)
