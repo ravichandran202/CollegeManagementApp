4fd26943144abd2621526a2f0e83755e67d2bebb
Ravichandran T S <ravichandrants202@gmail.com>
2023-11-04 19:09:34 +0530

    Added signin, signup, logout functionalities. using user creation form

--- collegeproject/collegeapp/urls.py
+++ collegeproject/collegeapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path("",views.home_page, name="home"),
     path("signin",views.signin_page, name="signin"),
-    path("signup",views.signup_page, name="signup")
+    path("signup",views.signup_page, name="signup"),
+    path("logout", views.logout, name="logout")
 ]
--- collegeproject/collegeapp/views.py
+++ collegeproject/collegeapp/views.py
@@ -1,11 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User, auth
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
     return render(request,"index.html")
 
 def signin_page(request):
+    if request.method == 'POST':
+        username = request.POST['username'].lower()
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, 'Username or Password is incorrect')
+            return redirect("signin")
+
     return render(request, "signin.html")
 
 def signup_page(request):
+    if request.method == 'POST':
+        username = request.POST['username'].lower()
+        email = request.POST['email']
+        password1 = request.POST['password']
+        password2 = request.POST['password2']
+        if password1 == password2:
+            if User.objects.filter(email=email).exists():
+                messages.error(request, "Email Already Exists")
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.error(request, "username Already Exists")
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username, email, password1)
+                user.save()
+                messages.info(request, "Account created successfully")
+                return redirect('signin')
+        else:
+            messages.error(request, "Please Enter same Password")
+            return redirect('signup')
     return render(request, "signup.html")
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
